17ac6204d5ea72a8a2c6e98be120eb9281e626bf
Hyunlang Ban <gusfkd2827@gmail.com>
2024-02-12 23:12:31 +0900

    Title: 줄 세우기 2, Time: 301ms, Memory: 29MB, Status: Passed - CodeTree

--- 240212/줄 세우기 2/line-up-students-2.py
+++ 240212/줄 세우기 2/line-up-students-2.py
@@ -2,8 +2,19 @@
 # 키가 동일하다면 몸무게가 더 큰 학생이 앞
 n = int(input())
 res = []
+heights = []
 
 
 for i in range(n):
     h, w = map(int, input().split())
-    res.append((h,w, i+1))
+    res.append((w,h, i+1))
+    heights.append(h)
+
+res = sorted(res, reverse=True)
+heights = sorted(list(set(heights)))
+
+for height in heights:
+    for r in res:
+        w, h, n = r
+        if h == height:
+            print(h, w, n)
