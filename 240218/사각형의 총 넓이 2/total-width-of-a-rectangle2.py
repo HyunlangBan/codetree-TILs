28c36394dfce30313685b07476dd730b3ed55524
Hyunlang Ban <gusfkd2827@gmail.com>
2024-02-18 10:06:18 +0900

    Title: 사각형의 총 넓이 2, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240218/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240218/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,13 +1,26 @@
+OFFSET = 100
+MAX_R = 200
+
 n = int(input())
+rects = [ tuple(map(int, input().split())) for _ in range(n) ]
+
+checked = [ [0]*MAX_R for _ in range(MAX_R) ]
+
+for rec in rects:
+    x1, y1, x2, y2 = rec
+    x1, y1 = x1+OFFSET, y1+OFFSET
+    x2, y2 = x2+OFFSET, y2+OFFSET
+
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            checked[i][j] = 1
+
 
 res = 0
-for _ in range(n):
-    x1, y1, x2, y2 = map(int, input().split())
-    x1 += 100
-    y1 += 100
-    x2 += 100
-    y2 += 100
+for i in range(MAX_R):
+    for j in range(MAX_R):
+        if checked[i][j]:
+            res += 1
 
-    res += abs((x2-x1)*(y2-y1)+'a')
 
 print(res)
